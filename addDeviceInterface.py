84691e743b587e74c9869da5fdc81aa45d302f9c
Brendan Jackman <jackmab1@cs.man.ac.uk>
2012-12-01 19:55:00 +0000

    addDevice can now mount the device

--- addDeviceInterface.py
+++ addDeviceInterface.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+#for linux: adding a new interface to the database
 import subprocess
 import sys
+import os
+
 import pyudev
 from pyudev import Context
 context = Context()
@@ -13,15 +16,30 @@ def get_partitions():
     return_list.append(partition)
   return return_list
 
-#for LINUX: takes a device name and returns where it is mounted to
-def dev_mount_point(device_name):
+#takes a device name and returns where it is mounted to
+def dev_mounted_at(device_name):
   mount_process = subprocess.Popen("mount", stdout=subprocess.PIPE)
   lines = mount_process.communicate()[0].split("\n")
   for line in lines:
-    print "searching for \"%s\" in: %s" % (device_name, line)
     if device_name in line:
       return line.split()[2]
   return None
+
+#takes a device name, mounts it, returns the mount point or none
+def mount(device):
+  device_name = device["DEVNAME"]
+  uuid = device["ID_FS_UUID"]
+  home = os.environ["HOME"]
+  mount_point = os.path.join(home, "." + "synchrotron-" + uuid)
+  if not os.path.exists(mount_point):
+    os.mkdir(mount_point)
+  mount_process = subprocess.Popen(["mount", device_name, mount_point])
+  mount_process.communicate()
+  if mount_process.returncode == 0:
+    return mount_point
+  else:
+    return None
+  
 
 available_partitions = get_partitions()
 
@@ -35,10 +53,12 @@ device_name = partition["DEVNAME"]
 
 #complain if the partition is already mounted 
 
-mount_point = dev_mount_point(device_name)
+mount_point = dev_mounted_at(device_name)
 if mount_point is not None:
-  sys.stderr.write("Device % is mounted at %s - unmount to continue\n"
+  sys.stderr.write("Device %s is mounted at %s - unmount to continue\n"
                    % (device_name, mount_point))
   sys.exit()
-else:
-  print "ok, not mounted"
+
+uuid = partition["ID_FS_UUID"]
+
+print mount(partition)
