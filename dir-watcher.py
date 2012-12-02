e801b63e53a40b557a26af3e497cb0dc29b58732
Brendan Jackman <jackmab1@cs.man.ac.uk>
2012-12-02 00:40:43 +0000

    added dummy dir watcher

---
+++ dir-watcher.py
@@ -0,0 +1,8 @@
+"""
+watch a directory (currently dummy)
+"""
+
+def start(devname):
+  print "pretending to watch %s" % devname
+def stop(devname):
+  print "stopping pretending to watch %s" % devname
