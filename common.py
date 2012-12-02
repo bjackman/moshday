dcc1c45374523c1b61c587eaa298ec7d813755ce
Brendan Jackman <jackmab1@cs.man.ac.uk>
2012-12-02 06:00:16 +0000

    added function to find mount point from UUID, and mount and unmount functions

--- common.py
+++ common.py
@@ -1,6 +1,16 @@
 """
-variables that are common to all modules
+helper variables/methods that are common to all modules
 """
+import subprocess
 import os
 
 default_db_path = os.path.join(os.environ["HOME"], ".synchrotron.xml")
+
+def mount(device_name, mount_point):
+  return subprocess.call(["mount", device_name, mount_point])
+def unmount(device_name):
+  return subprocess.call(["umount", device_name])
+
+#take a devices uuid and return the path that synchrotron will mount it to
+def mount_point(uuid):
+  return os.path.join(os.environ["HOME"], ".synchrotron-" + uuid)
