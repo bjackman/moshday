a0eac79205c04c7901d463d1d142da6cff01e3ca
Brendan Jackman <jackmab1@cs.man.ac.uk>
2012-12-02 03:40:10 +0000

    moved directory_watcher on from dummy stage - not yet working though

--- directory_watcher.py
+++ directory_watcher.py
@@ -1,8 +1,24 @@
 """
 watch a directory (currently dummy)
 """
+import database
+import pyinotify
 
-def start(devname):
-  print "pretending to watch %s" % devname
-def stop(devname):
-  print "stopping pretending to watch %s" % devname
+mask = pyinotify.IN_CREATE
+
+class INotifyCallback(pyinotify.ProcessEvent):
+  def process_IN_CREATE(self_event):
+    print "File created: " + event.pathname
+
+watch_manager = pyinotify.WatchManager()
+handler = INotifyCallback()
+notifier = pyinotify.Notifier(watch_manager, handler)
+
+#start and stop will be passed objects from database.SyncDevice
+def start(sync_device):
+  print sync_device
+  print "pretending to watch %s" % sync_device.sync_dir
+  watch_manager.add_watch(sync_device.sync_dir, mask, rec=True)
+
+def stop(sync_device):
+  print "stopping pretending to watch %s" % sync_device.sync_dir
